c6448eec942d9ca3405997a60440c57adabba28f
Gautham3348 <84218119+Gautham3348@users.noreply.github.com>
2022-05-22 19:42:18 +0530

    Create datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,14 @@
+class datatypes {
+    public static void main(String[] args) {
+        int hello = 10;
+        System.out.println(hello);
+        float hai = 20;
+        System.out.println(hai);
+        boolean hey = true;
+        System.out.println(hey);
+        char hlo = 'H';
+        System.out.println(hlo);
+        String hi = "Hello Java";
+        System.out.println(hi);
+    }
+}
