8193b9ded6ba9e7ca52c809870508b6d794b620f
Gautham3348 <84218119+Gautham3348@users.noreply.github.com>
2022-05-23 14:25:40 +0530

    Create elseif.java

---
+++ elseif.java
@@ -0,0 +1,19 @@
+class hey
+{
+    public static void main(String[] args)
+    {
+        int num = -15;
+        if(num>0)
+        {
+            System.out.println("Positive number");
+        }
+        else if(num<0)
+        {
+            System.out.println("Negative number");
+        }
+        else
+        {
+            System.out.println("Zero");
+        }
+    }
+}
