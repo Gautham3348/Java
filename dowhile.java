8f6a05bd0b87f683bed145c58a8460b42721ba3b
Gautham3348 <84218119+Gautham3348@users.noreply.github.com>
2022-05-23 15:47:07 +0530

    Create dowhile.java

---
+++ dowhile.java
@@ -0,0 +1,12 @@
+class dowhilelp
+{
+    public static void main(String[] args)
+    {
+        int i = 1;
+        System.out.println("The odd numbers between 20 is : ");
+        do{
+            System.out.println(i);
+            i = i+2;
+          }while (i<20);
+    }
+}
