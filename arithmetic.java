0ae995d7f153f157bf7a484ac190ac0ad317ef65
Gautham3348 <84218119+Gautham3348@users.noreply.github.com>
2022-05-23 10:46:40 +0530

    Create arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,26 @@
+class arithmetic
+{
+    public static void main(String[] args)
+    {
+        //arithmetic operators
+        int a=10, b=20, c;
+        {
+            c=a+b;
+            System.out.println("The additon of a and b is : "+c);
+        }
+        {
+            c=a-b;
+            System.out.println("The substraction of a and b is :"+c);
+        }
+        {
+            c=a*b;
+            System.out.println("The multiplication of a and b is : "+c);
+        }
+        {
+            c=b/a;
+            System.out.println("The division of a and b is : "+c);
+        }
+            c=a%b;
+            System.out.println("The modulus of two num is : "+c);
+    }
+}
