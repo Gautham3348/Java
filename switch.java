45153ad0bc684cc07d52c3da27c1a98a986793c3
Gautham3348 <84218119+Gautham3348@users.noreply.github.com>
2022-05-23 21:59:24 +0530

    Create switch.java

---
+++ switch.java
@@ -0,0 +1,21 @@
+class switchs
+{
+    public static void main(String[] args)
+    {
+        int num =2;
+        switch(num)
+        {
+            case 6:
+                System.out.println("no num");
+                break;
+            case 1:
+                System.out.println("No num");
+                break;
+            case 2:
+                System.out.println("This is number 2 ");
+                break;
+            default:
+                System.out.println("Invalid");
+        }
+    }
+}
