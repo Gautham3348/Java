b8b3645cd4ffd60e606f77c1fa9a9b41debce57f
Gautham3348 <84218119+Gautham3348@users.noreply.github.com>
2022-05-23 11:03:47 +0530

    Create relational.java

---
+++ relational.java
@@ -0,0 +1,18 @@
+class relatonalop
+{
+    public static void main(String[] args)
+    {
+        int a=10, b=20;{
+
+    }
+        System.out.print("Is a greater than b :");
+        System.out.println(a>b);
+        System.out.print("Is a less than b :");
+        System.out.println(a<b);
+        System.out.print("Is a equal to b : ");
+        System.out.println(a==b);
+        System.out.print("Is a not equal to b : ");
+        System.out.println(a!=b);
+
+    }
+}
