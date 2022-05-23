5d725f135e00e85df0c9e31dd51e0a6b7faa6b38
Gautham3348 <84218119+Gautham3348@users.noreply.github.com>
2022-05-23 19:32:29 +0530

    Create 1st function.java

---
+++ 1st function.java
@@ -0,0 +1,14 @@
+class funt
+{
+    public static void main(String[] args)
+    {
+        gautham();
+
+    }
+
+    public static void gautham()
+    {
+        System.out.println("Hi, This is gautham");
+
+    }
+}
