a735270769695dc31fe9732905bd9d3ddf9b2c1c
Gautham3348 <84218119+Gautham3348@users.noreply.github.com>
2022-06-06 10:42:11 +0530

    Create constructor.java

---
+++ constructor.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    int a, b;
+    public Main()
+    {
+        a=10;
+        b=20;
+    }
+    public static void main(String[] args)
+    {
+        Main comeon = new Main();
+        {
+            System.out.println(comeon.a+comeon.b);
+        }
+    }
+}
