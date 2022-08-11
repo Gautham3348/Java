855c06538efe8c010282653015e549fa6f19b3cf
Metta Gowtham Reddy <84218119+Gautham3348@users.noreply.github.com>
2022-08-11 11:10:08 +0530

    Create ElseifA.java

---
+++ ElseifA.java
@@ -0,0 +1,19 @@
+# Java
+class ElseifA
+{
+  public static void main(String[] args)
+  {
+    int x = 5;
+    if(x<3)
+    {
+      System.out.println("x is less than 3");
+    }
+    else if(x<6)
+    {
+      System.out.println("x is less than 6, and greater than the 3");
+    }
+    else{
+      System.out.println("x is greater than 5");
+    }
+  }
+}
